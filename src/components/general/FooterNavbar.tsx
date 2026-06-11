f548d58e55e17a661d7a15c97cc724ebc23df3c2
Jorge Sakaino <jorgesakaino@gmail.com>
2026-06-11 16:57:54 -0500

    skills carouel fixes

--- src/components/general/FooterNavbar.tsx
+++ src/components/general/FooterNavbar.tsx
@@ -8,11 +8,11 @@ import {
 export function FooterNavbar() {
   return (
     <NavigationMenu className="text-primary">
-      <NavigationMenuList>
+      <NavigationMenuList className="gap-0 lg:gap-1">
         <NavigationMenuItem className="hover:text-primary">
           <NavigationMenuLink
             href="/"
-            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-4 sm:px-8 pt-4 pb-2 sm:text-lg"
+            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-3 md:px-6 lg:px-8 pt-4 pb-2 sm:text-lg"
           >
             Inicio
           </NavigationMenuLink>
@@ -20,7 +20,7 @@ export function FooterNavbar() {
         <NavigationMenuItem className=" hover:text-primary">
           <NavigationMenuLink
             href="/development"
-            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-4 sm:px-8 pt-4 pb-2 sm:text-lg"
+            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-3 md:px-6 lg:px-8 pt-4 pb-2 sm:text-lg"
           >
             Desarrollo
           </NavigationMenuLink>
@@ -28,7 +28,7 @@ export function FooterNavbar() {
         <NavigationMenuItem className=" hover:text-primary">
           <NavigationMenuLink
             href="/design"
-            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-4 sm:px-8 pt-4 pb-2 sm:text-lg"
+            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-3 md:px-6 lg:px-8 pt-4 pb-2 sm:text-lg"
           >
             Diseño
           </NavigationMenuLink>
@@ -36,7 +36,7 @@ export function FooterNavbar() {
         <NavigationMenuItem className=" hover:text-primary">
           <NavigationMenuLink
             href="/contact"
-            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-4 sm:px-8 pt-4 pb-2 sm:text-lg"
+            className="hover:bg-background border-b-background rounded-none focus:bg-background focus:border-b-secondary active:border-b-secondary px-3 md:px-6 lg:px-8 pt-4 pb-2 sm:text-lg"
           >
             Contacto
           </NavigationMenuLink>
